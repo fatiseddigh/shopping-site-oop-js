b6967908306fad96f9cb9a4681627adebdb1e84d
fatiseddigh <seddighfati@gmail.com>
2023-06-04 14:09:04 +0430

    faet : add modal

---
+++ products.js
@@ -0,0 +1,17 @@
+const showModalBtn = document.querySelector(".show-modal");
+const closeModalBtn = document.querySelector(".close-modal");
+const modal = document.querySelector(".modal");
+const backDrop = document.querySelector(".backdrop");
+
+showModalBtn.addEventListener("click", showModal);
+closeModalBtn.addEventListener("click", closeModal);
+backDrop.addEventListener("click", closeModal);
+
+function showModal() {
+  modal.classList.remove("hidden");
+  backDrop.classList.remove("hidden");
+}
+function closeModal() {
+  modal.classList.add("hidden");
+  backDrop.classList.add("hidden");
+}
